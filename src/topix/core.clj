8c9450c9020acbb0c73e8e53cb17ca0e21565e15
rod <rod@pu-gh.com>
2012-04-16 22:24:27 +0100

    added basic form for topic

--- src/topix/core.clj
+++ src/topix/core.clj
@@ -4,7 +4,11 @@
   (:require [topix.layout :as layout]
             [noir.server :as server]))
 
-(defpage "/" [] layout/index-page)
+(defpage "/" [] 
+  (layout/index-page))
+
+(defpage "/show" {:keys [topic]}
+  (layout/show-page topic))
 
 (server/start 8080)
 
--- src/topix/layout.clj
+++ src/topix/layout.clj
@@ -10,15 +10,29 @@
     (page/include-css "/css/bootstrap.min.css")
     (page/include-css "/css/default.css")]
   [:body
-    [:div.main
-      [:div.span12 body
+    [:div.container
+      [:div.span12 
         [:h1 title]
+        [:div.content body]
         [:div.footer "by rod"]]
       (page/include-js "/js/bootstrap.min.js")]])
 
 ;; Public
 
-(defn index-page [_]
+(defn index-page []
   (layout "Welcome to Topix!"
-    (form/form-to [:get "/topic"])))
+    (form/form-to {:class "well"} [:get "show"]
+      (form/label "topic" "Choose a topic...")
+      (form/text-field {} "topic")
+      (form/submit-button {:class "btn"} "Show Topic"))))
+
+(defn show-page [topic]
+  (layout topic
+    (form/form-to {:class "well"} [:post "submit"]
+      (form/label "body" "Enter some text...")
+      (form/text-area {} "body")
+      [:label.checkbox 
+        (form/check-box {} "hit")
+        "Matches topic?"]
+      (form/submit-button {:class "btn"} "Add Example"))))
 
