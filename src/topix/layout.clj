f41d30bd16376a07a1c50c0407dac54af09dd226
rod <rod@pu-gh.com>
2012-04-16 21:58:15 +0100

    added the ubiquitous bootstrap

--- src/topix/layout.clj
+++ src/topix/layout.clj
@@ -1,8 +1,24 @@
 
-(ns topix.layout)
+(ns topix.layout
+  (:use noir.core)
+  (:require [hiccup.page :as page]
+            [hiccup.form :as form]))
+
+(defpartial layout [title & body]
+  [:head
+    [:title "T0P1X"]
+    (page/include-css "/css/bootstrap.min.css")
+    (page/include-css "/css/default.css")]
+  [:body
+    [:div.main
+      [:div.span12 body
+        [:h1 title]
+        [:div.footer "by rod"]]
+      (page/include-js "/js/bootstrap.min.js")]])
 
 ;; Public
 
 (defn index-page [_]
-  "INDEX")
+  (layout "Welcome to Topix!"
+    (form/form-to [:get "/topic"])))
 
